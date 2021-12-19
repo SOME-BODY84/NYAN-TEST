a0069af27ee6500918ba403aba3b8cd5c3ba2dd7
SOME-BODY84 <86975353+SOME-BODY84@users.noreply.github.com>
2021-12-19 20:45:11 +0100

    Update wallet.h

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -45,13 +45,13 @@ extern bool fWalletRbf;
 
 static const unsigned int DEFAULT_KEYPOOL_SIZE = 100;
 //! -paytxfee default
-static const CAmount DEFAULT_TRANSACTION_FEE = 10;
+static const CAmount DEFAULT_TRANSACTION_FEE = 0;
 //! -fallbackfee default
-static const CAmount DEFAULT_FALLBACK_FEE = 10;
+static const CAmount DEFAULT_FALLBACK_FEE = 1;
 //! -mintxfee default
-static const CAmount DEFAULT_TRANSACTION_MINFEE = 10;
+static const CAmount DEFAULT_TRANSACTION_MINFEE = 1;
 //! minimum recommended increment for BIP 125 replacement txs
-static const CAmount WALLET_INCREMENTAL_RELAY_FEE = 10;
+static const CAmount WALLET_INCREMENTAL_RELAY_FEE = 1;
 //! target minimum change amount
 static const CAmount MIN_CHANGE = CENT;
 //! final minimum change amount after paying for fees
